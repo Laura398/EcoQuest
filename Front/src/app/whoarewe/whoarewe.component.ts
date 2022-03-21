8c2d2b65e7d5fd79f6a0b4a044c41ab23317ca1a
Lucie Brackenier <lucie.brackenier@ynov.com>
2022-03-21 20:02:39 +0100

    résolution des conflits

--- Front/src/app/whoarewe/whoarewe.component.ts
+++ Front/src/app/whoarewe/whoarewe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageSpace } from '../models/image-space-model';
+
 
 @Component({
   selector: 'app-whoarewe',
@@ -8,8 +10,30 @@ import { Component, OnInit } from '@angular/core';
 export class WhoareweComponent implements OnInit {
 
   constructor() { }
+  imageEight!: ImageSpace;
+  imageNine!: ImageSpace;
+  imageTen!: ImageSpace;
+  imageEleven!: ImageSpace;
+
+ 
 
   ngOnInit(): void {
+    this.imageEight = {
+    imageUrl: '../assets/images/whoarewe/undraw_online_connection_6778.svg',
+    width: "400"
+ };
+ this.imageNine = {
+    imageUrl: '../assets/images/whoarewe/undraw_healthy_lifestyle_-6-tyl.svg',
+   width: "250"
+   
+ };
+    this.imageTen = {
+    imageUrl: '../assets/images/whoarewe/undraw_takeout_boxes_ap54.svg',
+    width: "250"
+    };
+    this.imageEleven = {
+    imageUrl: '../assets/images/whoarewe/undraw_environmental_study_re_q4q8.svg',
+    width: "250"
+ };
   }
 
-}
