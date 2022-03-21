d870238b2ea020f609c5d00616e01578870ca993
Lucie Brackenier <lucie.brackenier@ynov.com>
2022-03-21 08:41:11 +0100

    ajout des protips et du pied de page, fichier de la police rajoutée dans le css racine

--- Front/src/app/protips/protips.component.ts
+++ Front/src/app/protips/protips.component.ts
@@ -18,32 +18,45 @@ export class ProtipsComponent implements OnInit{
   imageSeven!:ImageSpace;
 
  ngOnInit(): void {
-   this.imageOne = new ImageSpace(
-    '../assets/images/protips/undraw_control_panel_re_y3ar.svg'
-   );
+   
+   this.imageOne = {
+    imageUrl: '../assets/images/protips/undraw_control_panel_re_y3ar.svg',
+    width: "600"
+ };
 
-   this.imageTwo = new ImageSpace(
-    '../assets/images/protips/undraw_flowers_vx06.svg'
-   );
+   this.imageTwo = {
+    imageUrl:'../assets/images/protips/undraw_flowers_vx06.svg',
+    width: "300",
+    title: "Planter"
+   };
 
-   this.imageThree = new ImageSpace(
-    '../assets/images/protips/undraw_biking_kc-4-f.svg'
-   );
+   this.imageThree = {
+    imageUrl:'../assets/images/protips/undraw_biking_kc-4-f.svg',
+    width: "300",
+    title: "Rouler à vélo"
+   };
 
-   this.imageFour = new ImageSpace(
-    '../assets/images/protips/undraw_throw_away_re_x60k.svg'
-   );
+   this.imageFour = {
+    imageUrl:'../assets/images/protips/undraw_throw_away_re_x60k.svg',
+    width: "300",
+    title: "Trier"
+   };
 
-   this.imageFive = new ImageSpace(
-    '../assets/images/protips/undraw_navigator_a479.svg'
-   );
+   this.imageFive = {
+    imageUrl:'../assets/images/protips/undraw_navigator_a479.svg',
+    width: "700"
+   };
 
-   this.imageSix = new ImageSpace(
-    '../assets/images/protips/undraw_moving_re_pipp.svg'
-   );
+   this.imageSix = {
+    imageUrl:'../assets/images/protips/undraw_moving_re_pipp.svg',
+    width: "350",
+    title: "Intérieur écologique"
+   };
 
-   this.imageSeven = new ImageSpace(
-    '../assets/images/protips/undraw_nature_on_screen_xkli.svg'
-   );
+   this.imageSeven = {
+    imageUrl:'../assets/images/protips/undraw_nature_on_screen_xkli.svg',
+    width: "300",
+    title: "Electronique reconditionné"
+   };
  }
 }
